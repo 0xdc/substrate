8b5fbb25cda01764582fc5d52e0379859ae4d0d8
Daniel Cordero (oak) <oak@0xdc.host>
2017-03-12 12:51:33 +0000

    Initial commit
    Script that runs catalyst over all stages for systemd

---
+++ catalyst.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+date=$(date +%Y%m%d)
+
+cleanup() {
+	test -e $tempstage && rm -f $tempstage
+	test -e $cataconf  && rm -f $cataconf
+}
+trap cleanup EXIT
+
+BASE_DIR=$(dirname $0)
+REPO_DIR=$BASE_DIR/releng
+BUILDS_DIR=$BASE_DIR/builds
+
+tempstage=$(mktemp)
+cataconf=$(mktemp)
+
+cat $BASE_DIR/catalyst.conf > $cataconf
+tee -a $cataconf <<<"storedir=\"$BASE_DIR\""
+tee -a $cataconf <<<"snapshot_cache=\"$BASE_DIR/snapshot_cache\""
+
+catalyst="catalyst -c $cataconf"
+
+if ! test -e $(dirname $0)/snapshots/portage-$date.tar.bz2; then
+	emaint sync -r gentoo
+	$catalyst -s $date
+fi
+
+for stage in stage1 stage2 stage3 stage4; do
+	sed "s:@REPO_DIR@:$REPO_DIR:;s/@latest@/$date/" \
+		$REPO_DIR/releases/weekly/specs/amd64/$stage-systemd.spec | \
+		tee $tempstage
+
+	$catalyst -f $tempstage
+
+	rm -f $BUILDS_DIR/systemd/$stage-amd64-systemd-latest.tar.bz2
+	ln -s $BUILDS_DIR/systemd/$stage-amd64-systemd-$date.tar.bz2 $BUILDS_DIR/systemd/$stage-amd64-systemd-latest.tar.bz2
+done
