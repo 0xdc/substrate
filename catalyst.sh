05fc612562b25f3b206482e5cbf3285470c7f1f8
0xdc <gitlab@0xdc.io>
2018-04-14 08:46:42 +0000

    Remove destructive cleanup

--- catalyst.sh
+++ catalyst.sh
@@ -5,13 +5,6 @@ set -e
 date=${1:-$(date --date=yesterday +%Y%m%d)}
 arch=$(uname -m)
 
-cleanup() {
-	test -e $tempstage && rm -f $tempstage
-	test -e $cataconf  && rm -f $cataconf
-	rm -fr $BASE_DIR/tmp
-}
-trap cleanup EXIT
-
 BASE_DIR=$(dirname $0)
 REPO_DIR=$BASE_DIR/weekly
 
