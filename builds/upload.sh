f2351752bb8518bc39143ea74247c9135d701665
Daniel Cordero (bastion) <bastion@azolla.0xdc.host>
2018-05-03 07:30:29 +0000

    Add tests for python-openstackclient and configuration

--- builds/upload.sh
+++ builds/upload.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+which openstack
+test "$OS_CLOUD" = "envvars" || (test -f /etc/openstack/clouds.yaml || test -f ~/.config/openstack/clouds.yaml )
+
 pushd $(dirname $0)
 
 openstack object create builds $(find */*/*/ */*/*.txt -type f -name '*stage[34]-*')
