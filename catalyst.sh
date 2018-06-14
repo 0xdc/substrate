e639e80d9c5439447932f1bc661bb5a32fb7350b
Daniel Cordero (bastion) <bastion@azolla.0xdc.host>
2018-06-14 13:43:08 +0000

    Disable stages that require bindist

--- catalyst.sh
+++ catalyst.sh
@@ -9,7 +9,7 @@ BASE_DIR="$(dirname $(readlink -f $0))"
 REPO_DIR=$BASE_DIR/weekly
 
 if test x"$arch" = "xx86_64"; then
-	targets="${TARGETS:-systemd:stage1 systemd:stage2 systemd:stage3 router:stage4 systemd:stage4 sso:stage4 plasma:stage4 plasma-sso:stage4}"
+	targets="${TARGETS:-systemd:stage1 systemd:stage2 systemd:stage3 router:stage4 systemd:stage4 sso:stage4}"
 	upstream="amd64"
 elif test x"$arch" = "xarmv7l"; then
 	targets="${TARGETS:-hardfp:stage1 hardfp:stage2 hardfp:stage3 hardfp:stage4}"
