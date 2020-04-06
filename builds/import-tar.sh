049b76ddd2a3d5d6fb55c42c38ffbfd5a8211418
Daniel Cordero <substrate@0xdc.io>
2020-04-06 18:01:25 +0000

    import-tar: only configure portage when it exists in the unpacked stage

--- builds/import-tar.sh
+++ builds/import-tar.sh
@@ -38,30 +38,32 @@ if test "$(machinectl show-image -p ReadOnly --value $name)" = "yes"; then
 	machinectl read-only ${name} false
 fi
 
-tee /var/lib/machines/${name}/etc/portage/make.conf <<EOF
-COMMON_FLAGS="-O2 -pipe"
-CFLAGS="\${COMMON_FLAGS}"
-CXXFLAGS="\${COMMON_FLAGS}"
-FCFLAGS="\${COMMON_FLAGS}"
-FFLAGS="\${COMMON_FLAGS}"
-DISTDIR="/var/cache/distfiles"
-PKGDIR="/var/cache/binpkgs"
-PORT_LOGDIR="/var/log/portage/build"
-LC_MESSAGES=C
+if test -d /var/lib/machines/${name}/etc/portage; then
+	tee /var/lib/machines/${name}/etc/portage/make.conf <<-EOF
+	COMMON_FLAGS="-O2 -pipe"
+	CFLAGS="\${COMMON_FLAGS}"
+	CXXFLAGS="\${COMMON_FLAGS}"
+	FCFLAGS="\${COMMON_FLAGS}"
+	FFLAGS="\${COMMON_FLAGS}"
+	DISTDIR="/var/cache/distfiles"
+	PKGDIR="/var/cache/binpkgs"
+	PORT_LOGDIR="/var/log/portage/build"
+	LC_MESSAGES=C
 
-FEATURES="\$FEATURES -news"
-FEATURES="\$FEATURES fail-clean"
-FEATURES="\$FEATURES buildpkg binpkg-multi-instance"
-EMERGE_DEFAULT_OPTS="\$EMERGE_DEFAULT_OPTS --quiet-build --usepkg=y"
-EMERGE_DEFAULT_OPTS="\$EMERGE_DEFAULT_OPTS --buildpkg-exclude '${excluded[@]}'"
-EOF
+	FEATURES="\$FEATURES -news"
+	FEATURES="\$FEATURES fail-clean"
+	FEATURES="\$FEATURES buildpkg binpkg-multi-instance"
+	EMERGE_DEFAULT_OPTS="\$EMERGE_DEFAULT_OPTS --quiet-build --usepkg=y"
+	EMERGE_DEFAULT_OPTS="\$EMERGE_DEFAULT_OPTS --buildpkg-exclude '${excluded[@]}'"
+	EOF
 
-test -d /var/lib/machines/${name}/etc/portage/repos.conf || mkdir -p /var/lib/machines/${name}/etc/portage/repos.conf
-tee /var/lib/machines/${name}/etc/portage/repos.conf/gentoo.conf <<EOF
-[gentoo]
-location = /var/db/repos/gentoo
-auto-sync = no
-EOF
+	test -d /var/lib/machines/${name}/etc/portage/repos.conf || mkdir -p /var/lib/machines/${name}/etc/portage/repos.conf
+	tee /var/lib/machines/${name}/etc/portage/repos.conf/gentoo.conf <<-EOF
+	[gentoo]
+	location = /var/db/repos/gentoo
+	auto-sync = no
+	EOF
+fi
 
 systemd-nspawn --quiet -M${name} systemctl --quiet enable systemd-networkd
 systemd-nspawn --quiet -M${name} systemctl --quiet enable systemd-resolved
