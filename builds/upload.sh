a7206e4d40f3d171f0fe663628e7641174277e34
Daniel Cordero <substrate@0xdc.io>
2020-06-25 10:57:27 +0000

    Add iso to upload list

--- builds/upload.sh
+++ builds/upload.sh
@@ -7,4 +7,4 @@ test "$OS_CLOUD" = "envvars" || (test -f /etc/openstack/clouds.yaml || test -f ~
 
 pushd $(dirname $0)
 
-openstack object create bindist $(find */*/*/ */*/*.txt -type f -name '*stage[34]-*' -o -name '*embedded-*')
+openstack object create bindist $(find */*/*/ */*/*.txt -type f -name '*stage[34]-*' -o -name '*embedded-*' -o -name '*.iso' -o -name '*-livecd-stage2-*')
