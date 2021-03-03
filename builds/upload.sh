3cddb3cc8493b4f17c02c4ec8c9694afa10d343a
Daniel Cordero <substrate@0xdc.io>
2021-03-03 19:51:29 +0000

    upload: create and upload sha256 checksums

--- builds/upload.sh
+++ builds/upload.sh
@@ -7,4 +7,10 @@ test "$OS_CLOUD" = "envvars" || (test -f /etc/openstack/clouds.yaml || test -f ~
 
 pushd $(dirname $0)
 
-openstack object create builds $(find */*/*/ */*/*.txt -type f -name '*stage[34]-*' -o -name '*embedded-*')
+for dir in */*/*/; do
+	pushd $dir
+	test -f SHA256SUMS || sha256sum $(find * -type f -name '*stage[134]-*' -o -name '*embedded-*') | tee SHA256SUMS
+	popd
+done
+
+openstack object create builds $(find */*/*/ */*/*.txt -type f -name '*stage[34]-*' -o -name '*embedded-*' -o -name SHA256SUMS)
