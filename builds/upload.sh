d35a4e0cac53775cd4fb845a0544f8b28f67c096
Daniel Cordero <substrate@0xdc.io>
2021-04-03 16:44:18 +0000

    builds/upload.sh: improvements to script
    signed .sha256 files for systemd-nspawn
    
    reusable find arguments

--- builds/upload.sh
+++ builds/upload.sh
@@ -7,10 +7,43 @@ test "$OS_CLOUD" = "envvars" || (test -f /etc/openstack/clouds.yaml || test -f ~
 
 pushd $(dirname $0)
 
+f=(
+	-type f
+
+	\(
+		# need to handle stage1-amd64-default
+		! -name '*stage1-amd64-systemd*'
+		-a
+		! -name '*stage1-armv7a_hardfp*'
+	\)
+	-a
+	\(
+		-name '*stage[134]-*'
+		-o
+		-name '*embedded-*'
+	\)
+)
+
 for dir in */*/*/; do
 	pushd $dir
-	test -f SHA256SUMS || sha256sum $(find * -type f -name '*stage[134]-*' -o -name '*embedded-*') | tee SHA256SUMS
+	test -f SHA256SUMS || {
+		sha256sums=$(find * "${f[@]}" | xargs --no-run-if-empty sha256sum)
+
+		test "${sha256sums}" && {
+			cat >SHA256SUMS <<<"$sha256sums"
+			#test -f SHA256SUMS.gpg || gpg --batch --detach-sign -o SHA256SUMS.gpg SHA256SUMS
+		}
+	}
+	for i in *; do
+		case "$i" in
+		SHA256SUMS|*.CONTENTS.gz|*.sha256|*.gpg)
+			;;
+		*)
+			test -f ${i}.sha256 || sha256sum $i | gpg --batch --clear-sign -o ${i}.sha256
+			;;
+		esac
+	done
 	popd
 done
 
-openstack object create builds $(find */*/*/ */*/*.txt -type f -name '*stage[34]-*' -o -name '*embedded-*' -o -name SHA256SUMS)
+find */*/*/ */*/*.txt "${f[@]}" -o -name 'SHA256SUMS*' | xargs --no-run-if-empty openstack object create builds
