54fab20d27adf446e7157328ef623b1ff8f86b3b
Daniel Cordero <substrate@0xdc.io>
2023-06-21 12:35:52 +0000

    duet/firefox: resist fingerprinting

--- weekly/root_overlays/duet/usr/lib64/firefox/browser/defaults/preferences/roflmaOS-prefs.js
+++ weekly/root_overlays/duet/usr/lib64/firefox/browser/defaults/preferences/roflmaOS-prefs.js
@@ -2,3 +2,5 @@ pref("gfx.webrender.all",		true);
 /* Enable VA-API */
 /* still might need MOZ_DISABLE_RDD_SANDBOX=1 */
 pref("media.ffmpeg.vaapi.enabled",	true);
+
+pref("privacy.resistFingerprinting",	true);
