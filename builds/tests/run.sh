50e89da01de805154ea89a9fafddd71770e9094b
Daniel Cordero <substrate@0xdc.io>
2024-01-04 14:45:00 +0000

    tests/run: remove i variable

--- builds/tests/run.sh
+++ builds/tests/run.sh
@@ -7,24 +7,20 @@ HOSTNAME="${1:-builds.roflmao.space}"
 
 which expect >/dev/null || exit 1
 
-i=systemd
-iso=builds/amd64/$i/latest-livecd-stage3-amd64-$i.iso
+iso=builds/amd64/systemd/latest-livecd-stage3-amd64-systemd.iso
 test -f $iso && timeout 10m expect -f "$TESTDIR"/livecd.ex $iso
 
 iso=builds/amd64/minimal/latest-systemd-amd64-minimal.iso
 test -f $iso && timeout 5m expect -f "$TESTDIR"/livecd.ex $iso
 
-i=minimal
-iso=builds/amd64/$i/latest-livecd-stage3-amd64-$i.iso
+iso=builds/amd64/minimal/latest-livecd-stage3-amd64-minimal.iso
 if test -f $iso; then
 	timeout 10m expect -f "$TESTDIR"/livecd.ex $iso --extra-args "real_init=/usr/lib/systemd/systemd"
 	timeout 5m expect -f "$TESTDIR"/router.ex ${HOSTNAME}
 fi
 
-i=plasma
-iso=builds/amd64/$i/latest-livecd-stage3-amd64-$i.iso
+iso=builds/amd64/plasma/latest-livecd-stage3-amd64-plasma.iso
 test -f $iso && timeout 15m expect -f "$TESTDIR"/livecd.ex $iso --memory 1024 --disk size=10
 
-i=duet
-iso=builds/amd64/$i/latest-livecd-stage3-amd64-$i.iso
-test -f $iso && timeout 15m expect -f "$TESTDIR"/$i.ex $iso --memory 2048 --disk size=20
+iso=builds/amd64/duet/latest-livecd-stage3-amd64-duet.iso
+test -f $iso && timeout 15m expect -f "$TESTDIR"/duet.ex $iso --memory 2048 --disk size=30
