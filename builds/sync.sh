6d5a8098957702bb19eb61e97c0a2b836da4ccde
Daniel Cordero <substrate@0xdc.io>
2024-01-05 09:58:20 +0000

    builds/sync: fix excluding latest tarball symlinks

--- builds/sync.sh
+++ builds/sync.sh
@@ -9,7 +9,7 @@ rsync_args=(
 	--include='*/'
 
 	--exclude="*livecd-stage2*"
-	--exclude="-latest.tar.*"
+	--exclude="*-latest.tar.*"
 	--include='*.tar.*'
 	--include='latest-*.txt'
 	--include='*.iso'
