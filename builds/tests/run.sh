1b96e7e21bcb2626ce86fa03ec92d603494d14bc
Daniel Cordero <substrate@0xdc.io>
2024-01-17 16:51:55 +0000

    test/runner: add a timeout multiplier
    In case of high system load that causes the tests to take longer, without failed tests taking too long.

--- builds/tests/run.sh
+++ builds/tests/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+MULT=${MULT:-1}
 TESTDIR="$(dirname $0)"
 HOSTNAME="${1:-builds.roflmao.space}"
 
@@ -11,19 +12,19 @@ which virt-install >/dev/null || exit 2
 trap "virsh pool-refresh default" EXIT
 
 iso=builds/amd64/systemd/latest-livecd-stage3-amd64-systemd.iso
-test -f $iso && timeout 10m expect -f "$TESTDIR"/livecd.ex $iso
+test -f $iso && timeout $((10 * $MULT))m expect -f "$TESTDIR"/livecd.ex $iso
 
 iso=builds/amd64/minimal/latest-livecd-stage3-amd64-minimal.iso
 if test -f $iso; then
-	timeout 10m expect -f "$TESTDIR"/livecd.ex $iso --extra-args "real_init=/usr/lib/systemd/systemd"
-	timeout 5m expect -f "$TESTDIR"/router.ex ${HOSTNAME}
+	timeout $((10 * $MULT))m expect -f "$TESTDIR"/livecd.ex $iso --extra-args "real_init=/usr/lib/systemd/systemd"
+	timeout $((5 * $MULT))m expect -f "$TESTDIR"/router.ex ${HOSTNAME}
 fi
 
 iso=builds/amd64/plasma/latest-livecd-stage3-amd64-plasma.iso
-test -f $iso && timeout 15m expect -f "$TESTDIR"/livecd.ex $iso --memory 1024 --disk size=10
+test -f $iso && timeout $((15 * $MULT))m expect -f "$TESTDIR"/livecd.ex $iso --memory 1024 --disk size=10
 
 iso=builds/amd64/duet/latest-livecd-stage3-amd64-duet.iso
-test -f $iso && timeout 15m expect -f "$TESTDIR"/duet.ex $iso --memory 2048 --disk size=30
+test -f $iso && timeout $((15 * $MULT))m expect -f "$TESTDIR"/duet.ex $iso --memory 2048 --disk size=30
 
 iso=builds/amd64/gnome/latest-livecd-stage3-amd64-gnome.iso
-test -f $iso && timeout 15m expect -f "$TESTDIR"/gnome.ex $iso --memory 2048 --disk size=30
+test -f $iso && timeout $((15 * $MULT))m expect -f "$TESTDIR"/gnome.ex $iso --memory 2048 --disk size=30
